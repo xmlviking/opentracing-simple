fc1236486d6bc5125c49b1ad2765e4df1bff9413
Cotter, Eric <eric_cotter@dell.com>
2020-09-08 16:09:55 -0500

    added some test files for opentracing

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/opentracing-simple
+
+go 1.15
+
+require github.com/opentracing/opentracing-go v1.2.0 // indirect
---
+++ simple-1/simple-tracing.go
@@ -0,0 +1,46 @@
+package tracing
+
+import (
+	"fmt"
+	"os"
+)
+
+/** Using the great work from
+		https://github.com/yurishkuro
+	I take no credit what so ever for this code
+ */
+
+package main
+
+import (
+"fmt"
+"os"
+
+"github.com/opentracing/opentracing-go/log"
+"github.com/xmlviking/opentracing-simple/lib/tracing"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		panic("ERROR: Expecting one argument")
+	}
+
+	tracer, closer := tracing.Init("hello-world")
+	defer closer.Close()
+
+	helloTo := os.Args[1]
+
+	span := tracer.StartSpan("say-hello")
+	span.SetTag("hello-to", helloTo)
+
+	helloStr := fmt.Sprintf("Hello, %s!", helloTo)
+	span.LogFields(
+		log.String("event", "string-format"),
+		log.String("value", helloStr),
+	)
+
+	println(helloStr)
+	span.LogKV("event", "println")
+
+	span.Finish()
+}
